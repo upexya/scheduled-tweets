bf5332325ce595c18d41eb5906d8d35b685b3402
upexya <iupeachya@gmail.com>
2025-04-14 13:43:59 +0100

    handle user registration

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root to: "main#index"
 
   get "about", to: "about#index"
+
+  get "sign-up", to: "registrations#new"
+
+  post "sign-up", to: "registrations#create"
 end
