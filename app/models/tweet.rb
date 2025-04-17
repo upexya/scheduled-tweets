bf1c247d71a31607a20cbf44da9c4749f52a058e
upexya <iupeachya@gmail.com>
2025-04-17 14:49:26 +0100

    display scheduled tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,15 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: { maximum: 280, minimum: 1 }
+  validates :publish_at, presence: true
+
+  after_initialize do
+    self.publish_at ||= 24.hours.from_now
+  end
+
+  def published?
+    publish_at <= Time.current
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+  validates :password, presence: true, length: { minimum: 4 }, confirmation: true
 end
