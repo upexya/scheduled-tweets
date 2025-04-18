4669b09ff94804dfaa8bcfc124bf12170bbde7a3
upexya <iupeachya@gmail.com>
2025-04-18 17:01:53 +0100

    dependent destroy model for tweets

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,6 +1,7 @@
 class TwitterAccount < ApplicationRecord
   belongs_to :user
-  has_many :tweets
+  # dependent: :destroy -- this will delete all tweets associated with the account when the account is deleted
+  has_many :tweets, dependent: :destroy
 
   validates :username, uniqueness: true
 
